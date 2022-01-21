bb753bbcb8f4d57bf906f7ab2af33e00ce0d58b0
Damilare Anjorin <damilareanjorin1@gmail.com>
2022-01-21 20:17:07 +0100

    chore: update vue config

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -3,7 +3,20 @@ module.exports = {
   'plugin:vue/vue3-recommended',
   '@damilaredev/eslint-config-typescript',
  ],
- overrides: [],
+ overrides: [
+  {
+   files: ['*.vue'],
+   parser: 'vue-eslint-parser',
+   parserOptions: {
+    parser: '@typescript-eslint/parser',
+   },
+   rules: {
+    'no-unused-vars': 'off',
+    'no-undef': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+   },
+  },
+ ],
  rules: {
   'vue/require-default-prop': 'off',
   'vue/multi-word-component-names': 'off',
