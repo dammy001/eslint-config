07c7e00371ea3e250666da960540a25ecf12798a
Damilare Anjorin <damilareanjorin1@gmail.com>
2023-07-15 00:56:27 +0100

    chore: add sourceType

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -9,6 +9,7 @@ module.exports = {
       parser: 'vue-eslint-parser',
       parserOptions: {
         parser: '@typescript-eslint/parser',
+        sourceType: 'module',
       },
       rules: {
         'no-unused-vars': 'off',
